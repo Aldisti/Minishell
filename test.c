5fdf2ca484b8d6913aaf49c115ca640215a8c793
Aldisti <Aldisti@Legion5>
2023-03-21 14:47:13 +0100

    Automated push

--- test.c
+++ test.c
@@ -2,28 +2,11 @@
 
 int	main(void)
 {
-	char	*line;
-	char	*tmp;
+	char	*str;
 
-	while (42)
-	{
-		line = readline("$> ");
-		add_history(line);
-		if (!strncmp(line, "cd", 2))
-			chdir(line + 3);
-		else if (!strcmp(line, "pwd"))
-		{
-			tmp = getcwd(0, 0);
-			printf("%s\n", tmp);
-			free(tmp);
-		}
-		else if (!strcmp(line, "exit"))
-		{
-			rl_clear_history();
-			free(line);
-			break ;
-		}
-		free(line);
-	}
-	return (0);
+	str = getenv("OLDPWD");
+	if (str)
+		printf("%s\n", str);
+	else
+		printf("nonono\n");
 }
