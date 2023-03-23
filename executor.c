d5e1d71fb9da64702a1ab1bac62ab7d3e64c9083
Aldisti <Aldisti@Legion5>
2023-03-23 17:28:04 +0100

    Automated push

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 10:35:10 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/03/23 16:23:00 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:27:36 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ char	*ft_resolve_expansion(t_list *list, char *str, int lvl)
 {
 	t_env	tmp;
 
+	printf("resolve: |%s|\n", str);
 	if (!str || !*str)
 		return (ft_strdup(""));
 	if (!ft_strncmp(str, "$", ft_strlen(str)))
@@ -84,28 +85,67 @@ int	ft_check_dollar(char *str)
 // 	}
 // }
 
+// char	*ft_expansion(t_shell *shell, char *str)
+// {
+// 	int		i;
+// 	int		brackets;
+// 	char	*strs[4];
+// 	char	*dollar;
+
+// 	brackets = 0;
+// 	strs[3] = 0;
+// 	// str = ft_expand_tilde(shell->list, str);
+// 	while (ft_check_dollar(str) != -1)
+// 	{
+// 		dollar = str + ft_check_dollar(str);
+// 		i = 1;
+// 		while (dollar[i] && !ft_ifin(dollar[i], "$ ()'\""))
+// 			i++;
+// 		str = 
+// 		strs[0] = ft_substr(str, 0, dollar - str);
+// 		strs[2] = ft_substr(dollar, i, ft_strlen(dollar) - i + 1);
+// 		brackets = ft_countn(str, '(', ft_check_dollar(str)) - ft_countn(str, ')', ft_check_dollar(str));
+// 		// printf("brackets in |%s| at %d i are %d\n", str, ft_check_dollar(str), brackets);
+// 		strs[1] = ft_resolve_expansion(shell->list, ft_substr(dollar, 1, i - 1), brackets);
+// 		// free(str);
+// 		str = ft_joiner(strs, 1);
+// 	}
+// 	return (str);
+// }
+
+void	ft_check_quote(char *str, int *i)
+{
+	static int	quotes;
+	
+	if (str[*i] == '\"' && !quotes)
+		quotes = 1;
+	else if (str[*i] == '\"' && quotes)
+		quotes = 0;
+	if (str[*i] == '\'' && !quotes)
+		while (str[++(*i)] != 39)
+			;
+}
+
 char	*ft_expansion(t_shell *shell, char *str)
 {
-	int		i;
-	int		brackets;
+	int		index[2];
 	char	*strs[4];
-	char	*dollar;
 
-	brackets = 0;
+	index[0] = -1;
 	strs[3] = 0;
-	// str = ft_expand_tilde(shell->list, str);
-	while (ft_check_dollar(str) != -1)
+	while (str[++index[0]])
 	{
-		dollar = str + ft_check_dollar(str);
-		i = 1;
-		while (dollar[i] && !ft_ifin(dollar[i], "$ ()'\""))
-			i++;
-		str = 
-		strs[0] = ft_substr(str, 0, dollar - str);
-		strs[2] = ft_substr(dollar, i, ft_strlen(dollar) - i + 1);
-		brackets = ft_countn(str, '(', ft_check_dollar(str)) - ft_countn(str, ')', ft_check_dollar(str));
-		// printf("brackets in |%s| at %d i are %d\n", str, ft_check_dollar(str), brackets);
-		strs[1] = ft_resolve_expansion(shell->list, ft_substr(dollar, 1, i - 1), brackets);
+		ft_check_quote(str, &index[0]);
+		if (str[index[0]] != '$')
+			continue ;
+		strs[0] = ft_substr(str, 0, index[0]);
+		index[1] = index[0] + 1;
+		while (str[index[1]] && !ft_ifin(str[index[1]], "$ ()\'\""))
+			index[1]++;
+		strs[2] = ft_substr(str, index[1], ft_strlen(str) - index[1]);
+		strs[1] = ft_resolve_expansion(shell->list, ft_substr(str, index[0] + 1, index[1] - index[0] - 1),
+				(ft_countn(str, '(', ft_check_dollar(str)) - ft_countn(str, ')',
+				ft_check_dollar(str))));
 		// free(str);
 		str = ft_joiner(strs, 1);
 	}
@@ -139,5 +179,5 @@ int	main(void)
 	shell.list->next = 0;
 	// list2.content = &env2;
 	// list2.next = 0;
-	printf("\n<%s>\n", ft_expansion(&shell, "$HOME(('$HOME')$HOME)"));
+	printf("\n<%s>\n", ft_expansion(&shell, "$HOME((''$HOME'')\"$HOME\")"));
 }
