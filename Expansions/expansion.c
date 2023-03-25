c2350109758453694d9dd667e374c36e03470596
Aldisti <Aldisti@Legion5>
2023-03-25 18:20:28 +0100

    Automated push

--- Expansions/expansion.c
+++ Expansions/expansion.c
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 10:56:40 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/03/25 18:05:37 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/03/25 18:18:34 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,33 @@ char	*ft_expand_tilde(t_shell *shell, char *str, int i)
 	return (tmp);
 }
 
+char	*ft_expand_special(t_shell *shell, char *str, int i)
+{
+	char	*strs[4];
+	
+	if (!str[i + 1])
+	{
+		str[i + 1] = 0;
+		return (str);
+	}
+	strs[3] = 0;
+	strs[0] = ft_substr(str, 0, i);
+	if (!strs[0] && i > 0)
+		exit(1); // ft_die(shell)
+	strs[2] = ft_substr(str, i + 2, ft_strlen(str));
+	if (!strs[1] && str[i + 2])
+		exit(1); // ft_die(shell)
+	if (str[i + 1] == '?')
+		strs[1] = ft_itoa(g_shell_errno);
+	else if (str[i + 1] == '$')
+		strs[1] = ft_strdup("");
+	if (!strs[1])
+		exit(1); // ft_die(shell)
+	strs[3] = ft_joiner(strs, 1);
+	free(str);
+	return (strs[3]);
+}
+
 char	*ft_expand_dollar(t_shell *shell, char *str, int i)
 {
 	char	*strs[4];
@@ -52,11 +79,10 @@ char	*ft_expand_dollar(t_shell *shell, char *str, int i)
 
 	if (ft_getquotes(str, i) == 1)
 		return (str);
-	// if (str[i + 1] && (str[i + 1] == '?' || str[i + 1] == '$'))
-	// 	return (ft_expand_special(shell, str, i));
+	if (str[i + 1] && (str[i + 1] == '?' || str[i + 1] == '$'))
+		return (ft_expand_special(shell, str, i));
 	strs[3] = 0;
 	tmp = ft_getname(str, i);
-	// printf("|%s|\n", tmp);
 	if (!tmp)
 		exit(1); // ft_die(shell)
 	env = ft_search_in_list(shell->list, tmp, ft_getlvl(str, i));
@@ -66,18 +92,14 @@ char	*ft_expand_dollar(t_shell *shell, char *str, int i)
 	if (!strs[0] && i > 0)
 		exit(1); // ft_die(shell)
 	strs[2] = ft_substr(str, i + ft_strlen(tmp) + 1, ft_strlen(str));
-	// printf("right: |%s|\n", &str[ft_strlen(tmp) + i - 1]);
 	if (!strs[1] && ft_strlen(env->value))
 		exit(1); // ft_die(shell)
 	strs[1] = ft_strdup(env->value);
 	if (!strs[1] && ft_strlen(env->value))
 		exit(1); // ft_die(shell)
-	// printf("ciao\n");
 	free(tmp);
 	tmp = ft_joiner(strs, 1);
 	free(str);
-	// printf("|%s|\n", tmp);
-	// exit(1);
 	return (tmp);
 }
 
