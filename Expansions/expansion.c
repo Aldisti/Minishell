f6f678691cf025117264932ca0542372d6191f86
Aldisti <alessandrodiste99@gmail.com>
2023-04-01 16:37:41 +0200

    risolto un SIGSEGV e ottimizzate alcune funzioni

--- Expansions/expansion.c
+++ Expansions/expansion.c
@@ -43,26 +43,25 @@ char	*ft_expand_tilde(t_shell *shell, char *str, int i)
 	return (tmp);
 }
 
-char	*ft_expand_special(t_shell *shell, char *str, int i)
+char	*ft_expand_spec(t_shell *shell, char *str, int i)
 {
 	char	*strs[4];
 	
-	if (!str[i + 1])
-	{
-		str[i + 1] = 0;
-		return (str);
-	}
 	(void)shell;
+	if (!str[i + 1])
+		return (str);
 	strs[3] = 0;
 	strs[0] = ft_substr(str, 0, i);
 	if (!strs[0] && i > 0)
 		exit(1); // ft_die(shell)
 	strs[2] = ft_substr(str, i + 2, ft_strlen(str));
-	if (!strs[1] && str[i + 2])
+	if (!strs[2] && str[i + 2])
 		exit(1); // ft_die(shell)
 	if (str[i + 1] == '?')
 		strs[1] = ft_itoa(g_shell_errno);
 	else if (str[i + 1] == '$')
+		strs[1] = ft_strdup("$");
+	else
 		strs[1] = ft_strdup("");
 	if (!strs[1])
 		exit(1); // ft_die(shell)
@@ -71,62 +70,57 @@ char	*ft_expand_special(t_shell *shell, char *str, int i)
 	return (strs[3]);
 }
 
-char	*ft_expand_dollar(t_shell *shell, char *str, int i)
+char	*ft_expand_doll(t_shell *shell, char *str, int i)
 {
-	char	*strs[4];
-	char	*tmp;
+	char	*strs[4]; // se la faccio statica posso togliere 3 righe :)
 	t_env	*env;
 
-	strs[3] = 0;
-	tmp = ft_getname(str, i);
-	if (!tmp)
+	strs[0] = 0;
+	strs[1] = 0;
+	strs[2] = 0;
+	strs[3] = ft_getname(str, i);
+	if (!strs[3])
 		exit(1); // ft_die(shell)
-	env = ft_search_in_list(shell->list, tmp, ft_getlvl(str, i));
+	env = ft_search_in_list(shell->list, strs[3], ft_getlvl(str, i));
 	if (!env)
 		strs[1] = ft_strdup("");
 	else
 		strs[1] = ft_strdup(env->value);
-	if (!strs[1] && env)
-		exit(1); // ft_die(shell)
 	strs[0] = ft_substr(str, 0, i);
-	if (!strs[0] && i > 0)
+	strs[2] = ft_substr(str, i + ft_strlen(strs[3]) + 1, ft_strlen(str));
+	if (!strs[0] || !strs[1] || !strs[2])
 		exit(1); // ft_die(shell)
-	strs[2] = ft_substr(str, i + ft_strlen(tmp) + 1, ft_strlen(str));
-	if (!strs[2] && (ft_strlen(str) - i - ft_strlen(tmp) - 1))
-		exit(1); // ft_die(shell)
-	free(tmp);
-	tmp = ft_joiner(strs, 1);
-	free(str);
-	return (tmp);
+	ft_free((void **)&strs[3]);
+	strs[3] = ft_joiner(strs, 1);
+	ft_free((void **)&str);
+	return (strs[3]);
 }
 
-char	*ft_expansion(t_shell *shell, char *str)
+void	ft_expand_all(t_shell *shell)
 {
-	int		i;
+	int	i;
+	int	j;
+	int	quotes;
 
-	i = -1;
-	while (str[++i])
+	j = -1;
+	while (shell->parsed[++j])
 	{
-		if (str[i] == '~' && !ft_getquotes(str, i))
-			str = ft_expand_tilde(shell, str, i);
-		else if (str[i] == '$' && (!str[i + 1] || str[i + 1] == '?' || str[i + 1] == '$'))
-			str = ft_expand_special(shell, str, i);
-		else if (str[i] == '$' && ft_getquotes(str, i) != 1)
-			str = ft_expand_dollar(shell, str, i);
+		i = -1;
+		while (shell->parsed[j][++i])
+		{
+			quotes = ft_getquotes(shell->parsed[j], i);
+			if (shell->parsed[j][i] == '~' && !quotes)
+				shell->parsed[j] = ft_expand_tilde(shell, shell->parsed[j], i);
+			else if (shell->parsed[j][i] == '$' && quotes != 1
+					&& (ft_in(shell->parsed[j][i + 1], "*@#?-$!0")
+					|| !shell->parsed[j][i + 1]))
+				shell->parsed[j] = ft_expand_spec(shell, shell->parsed[j], i);
+			else if (shell->parsed[j][i] == '$' && quotes != 1)
+				shell->parsed[j] = ft_expand_doll(shell, shell->parsed[j], i);
+			else
+				continue ;
+			if (i >= ft_strlen(shell->parsed[j]))
+				i = ft_strlen(shell->parsed[j]) - 1;
+		}
 	}
-	return (str);
-}
-
-char	**ft_expand_all(t_shell *shell)
-{
-	int		i;
-	char	*tmp;
-
-	i = -1;
-	while (shell->parsed[++i])
-	{
-		tmp = ft_expansion(shell, shell->parsed[i]);
-		shell->parsed[i] = tmp;
-	}
-	return (shell->parsed);
 }
