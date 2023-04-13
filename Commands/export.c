93594e5b7d2b8063b4ae0b56b701106149b8deee
Aldisti <alessandrodiste99@gmail.com>
2023-04-13 15:02:10 +0200

    gestiti export e unset nelle pipe

--- Commands/export.c
+++ Commands/export.c
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 17:16:39 by adi-stef          #+#    #+#             */
-/*   Updated: 2023/04/13 11:05:40 by adi-stef         ###   ########.fr       */
+/*   Updated: 2023/04/13 15:01:45 by adi-stef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ int	ft_export(t_shell *shell, char **cmd, int lvl)
 
 	if (!cmd[1])
 		return (ft_print_export(shell, lvl));
+	if (shell->n_cmds != 1)
+		return (0);
 	while (++cmd && *cmd)
 	{
 		ft_remove_quotes(shell, cmd);
